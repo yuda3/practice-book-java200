47e15023fb835a3b60a491f7707b9b205f93bc69
yuda3 <yuda3@hotmail.com>
2021-02-10 23:13:09 +0900

    Billboard 10

---
+++ src/main/java/ridibooks/java200/chapter03/BillboardTimeMain.java
@@ -0,0 +1,36 @@
+package ridibooks.java200.chapter03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class BillboardTimeMain {
+    public static void main(String[] args) throws Exception {
+        String newUrls = "https://www.billboard.com/charts/hot-100/";
+        String s = "";
+        URL url = null;
+        try{
+            url  = new URL(newUrls);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8), 8);
+            String line = "";
+            while((line = reader.readLine()) != null){
+                if((!line.trim().equals(""))){
+                    if(line.trim().contains("<time datetime =")){
+                        s = line.trim();
+                        s = s.substring(0, s.indexOf(">") -1);
+                        s = s.substring(0, s.indexOf("\"") +1).trim();
+                        break;
+
+                    }
+                }
+            }
+        }catch (Exception e){
+            System.out.println("Billboard Parsing error");
+        }
+        System.out.println(s);
+    }
+
+}
