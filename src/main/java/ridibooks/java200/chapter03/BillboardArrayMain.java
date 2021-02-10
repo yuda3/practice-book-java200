36af6513be4c6f24ab2966564f84ab7d322da3e5
yuda3 <yuda3@hotmail.com>
2021-02-10 23:07:39 +0900

    Billboard 09

--- src/main/java/ridibooks/java200/chapter03/BillboardArrayMain.java
+++ src/main/java/ridibooks/java200/chapter03/BillboardArrayMain.java
@@ -1,26 +1,31 @@
 package ridibooks.java200.chapter03;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class BillboardArrayMain {
-    public static void main(String[] args) {
-        ArrayList<Billboard> bills = new ArrayList<>();
-        bills.add(new Billboard(1, "Despacito", 1, "https://www.billboard.com/images/pref_images/q61808osztw.jpg", "luis fonsi"));
-        bills.add(new Billboard(2, "That's What I Like", 2, "https://www.billboard.com/images/pref_images/q59725qvpol.jpg", "bruno mars"));
-        bills.add(new Billboard(3, "I am the one", 3, "https://www.billboard.com/images/pref_images/q64532p164ztw.jpg", "jd khaled"));
-
-        System.out.println(bills.size());
-        showAbout(bills.get(0));
-        bills.remove(1);
-        System.out.println("bills.size() = " + bills.size());
-        System.out.println("--------------------");
-        for (Billboard bill : bills) {
-            showAbout(bill);
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> bills = new ArrayList<>();
+        String newUrls = "https://www.billboard.com/charts/hot-100/";
+        URL url = null;
+        try{
+            url  = new URL(newUrls);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8), 8);
+            String line = null;
+            while((line = reader.readLine()) != null){
+                if(!line.trim().equals("")){
+                    bills.add(line.trim());
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
-    }
-
-    private static void showAbout(Billboard bb) {
-        String sf = String.format("%d, %s, %d, %s, %s", bb.getRank(), bb.getSong(), bb.getLastWeek(), bb.getImageSrc(), bb.getArtist());
-        System.out.println(sf);
+        for (String bill : bills) {
+            System.out.println(bill);
+        }
     }
 }
